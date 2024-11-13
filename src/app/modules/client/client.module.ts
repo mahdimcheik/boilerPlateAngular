07859a39798f54c0fb51cfd2ab7e9e62c57e88c5
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2024-11-14 00:10:22 +0100

    negin notifcation

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -14,6 +14,7 @@ import { AdressesComponent } from './pages/adresses/adresses.component';
 import { FormationComponent } from './components/formation/formation.component';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { ButtonModule } from 'primeng/button';
     PersonnalInfosComponent,
     AdressesComponent,
     FormationComponent,
+    NotificationComponent,
   ],
   imports: [
     CommonModule,
