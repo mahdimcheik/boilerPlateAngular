1cc4d8435550320bf3ff1f1b80f2ab15c62fc48a
Mahdi Mcheik <mahdi.mcheik@beecoming.fr>
2025-02-11 17:49:57 +0100

    style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { MessageService } from 'primeng/api';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TokenInterceptor } from './utilities/interceptors/token.interceptor';
 import { errorHandlerInterceptor } from './utilities/interceptors/error-handler.interceptor';
-import { LandingComponent } from './landing/landing.component';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
@@ -32,13 +31,7 @@ import { ThemeSelectorComponent } from './components/theme-selector/theme-select
 import localeFr from '@angular/common/locales/fr';
 registerLocaleData(localeFr);
 @NgModule({
-  declarations: [
-    AppComponent,
-
-    HomeComponent,
-    LandingComponent,
-    AdresseTypePipe,
-  ],
+  declarations: [AppComponent, HomeComponent, AdresseTypePipe],
   imports: [
     BrowserModule,
     AppRoutingModule,
